62daffdb9382b8010c4b93fbdb96e4c87c962634
Cristhoper <cristhoper@gmail.com>
2019-03-19 15:53:32 -0300

    Add init method

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ from threading import Thread, RLock
 from flask import Flask, request, abort
 from tbkpos import TbkPos
 
-GET = ['GET']
-POST = ['POST']
-
 DEVICE = "COM3"
 IP = "0.0.0.0"
 PORT = 4001
@@ -19,7 +16,15 @@ transactions_in_progress = {}
 safe_pos = RLock()
 
 
-def worker_sale(amount, transaction_id, dummy):
+def worker_init():
+    safe_pos.acquire()
+    print("Init started")
+    pos.close()
+    print("init ended")
+    safe_pos.release()
+
+
+def worker_sale(amount, transaction_id, dummy=False):
     safe_pos.acquire()
     print("Worker started")
     transactions_in_progress[transaction_id] = None
@@ -29,7 +34,7 @@ def worker_sale(amount, transaction_id, dummy):
     safe_pos.release()
 
 
-def pos_on_thread(amount, transaction_id, dummy=False):
+def payment_on_thread(amount, transaction_id, dummy=False):
     th_pos = Thread(target=worker_sale, args=(amount, transaction_id, dummy,))
     th_pos.daemon = True
     try:
@@ -38,7 +43,17 @@ def pos_on_thread(amount, transaction_id, dummy=False):
         print("Issues in POS: {}".format(err.message))
 
 
-@app.route("/payment", methods=POST)
+@app.route("/init", methods=['POST', 'GET'])
+def init():
+    th_pos = Thread(target=worker_init)
+    th_pos.daemon = True
+    try:
+        th_pos.start()
+    except RuntimeError as err:
+        print("Issues in POS: {}".format(err.message))
+
+
+@app.route("/payment", methods=['POST'])
 def payment():
     """
     POST /payment:
@@ -59,14 +74,14 @@ def payment():
     dummy = hasattr(data, 'dummy')
     if request.method in POST:
         if t_id not in transactions_in_progress.keys():
-            pos_on_thread(amount, t_id, dummy)
+            payment_on_thread(amount, t_id, dummy)
             return "ACK"
         else:
             return "BUSY"
     return "NAK"
 
 
-@app.route("/check/<transaction_id>", methods=GET)
+@app.route("/check/<transaction_id>", methods=['GET'])
 def check(transaction_id):
     """
     GET /check/<transaction_id>:
@@ -76,12 +91,12 @@ def check(transaction_id):
     "transaction_id": string,
     "approval_code": string,
     "print_data": string,
+    ...
     }
     """
     if transaction_id is None:
         return abort(404)
 
-    status = None
     transaction_id = int(transaction_id)
     if transaction_id in transactions_in_progress.keys():
         status = transactions_in_progress[transaction_id]
