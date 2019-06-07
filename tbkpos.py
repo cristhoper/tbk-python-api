61131a64ea06fe0f51dd690c73bae5f1478cb55d
cristhoper <cristhoper@gmail.com>
2019-06-07 02:00:35 -0400

    FIX polling and sale

--- tbkpos.py
+++ tbkpos.py
@@ -6,25 +6,35 @@ from transaction import TransactionData
 from time import sleep
 
 class TbkPos(object):
-    lock = RLock()
-    TERMINATOR = '\r'
+    lock = RLock()    
 
-    def __init__(self, device, baudrate=9600):
+    def __init__(self, device, baudrate=115200):
         self.ser = Serial(device, baudrate=baudrate, timeout=3)
         self.device = device
+        COD_COMER = ''
+        TER_ID = ''
 
     def __execute(self, command):
         self.lock.acquire()
+        print("Sending message {}".format(command))
+        self.ser.flush()
         self.ser.write(command)
-        val = ''
+        val = self.ser.readall()
         cnt = 0
         while len(val) <=0 and cnt < MAX_ATTEMPT:
             print("Sending message {}".format(command))
+            self.ser.write(command)
             val = self.ser.readall()
             cnt += 1
         self.lock.release()
         print("Received message {}".format(val))
-        return val
+        return val.decode('utf-8')
+
+    def __wait_data(self):
+        val = ''
+        while len(val) <= 0:
+            val = self.ser.readall()
+        return val.decode('utf-8')
 
     @staticmethod
     def __get_properties(token):
@@ -41,11 +51,13 @@ class TbkPos(object):
         flag = False
         if len(in_stream) <= 0:
             return flag
-        data = in_stream.split("\\|")
+        print(in_stream)
+        data = str(in_stream).split('|')
+        print(data)
         for token in data:
             if i == flag_number:
                 flag = token
-                i += 1
+            i += 1
         if not flag and flag_number == VENTA_TX_MONTO_CUOTA:
             flag = "0"
         if flag_number == TX_TERMINAL_ID:
@@ -82,6 +94,27 @@ class TbkPos(object):
             print("More errors: {}".format(err))
         return obj
 
+    def load_keys(self):
+        obj = TransactionData()
+        cmd = STX + "0800" + ETX
+        cmd_hex = posutils.hex_string(cmd, crc=True)
+        try:
+            result = obj.set_response(self.__execute(cmd_hex))
+            if result[0] == ACK:
+                result = obj.set_response(self.__wait_data()[2:-2])
+            flag = self.__get_flags(result, TX_RESPUESTA)
+            obj.set_response_code(flag)
+            if flag == "00":
+                obj.result = True
+                obj.add_content("codigo_comercio", self.__get_flags(result, TX_CODIGO_COMERCIO))
+                obj.add_content("terminal_id", self.__get_flags(result, TX_TERMINAL_ID))
+                self.COD_COMER = obj.get_content("codigo_comercio")
+                self.TER_ID = obj.get_content("terminal_id")
+        except IOError as err:
+            print("More errors: {}".format(err))
+        self.ack()
+        return obj
+                                              
     def polling(self):
         print("POS POLLING...")
         obj = TransactionData()
@@ -94,11 +127,49 @@ class TbkPos(object):
                 obj.set_text("Conexion establecida en puerto: {}".format(str(self.device)))
             else:
                 obj.set_text("Puerto incorrecto, intente con otro puerto.")
+        except IOError as err:
+            print("More errors: {}".format(err))
+        self.ack()
+        return obj
+
+    def sale_init(self, amount, voucher='0', dummy=False):  # , **kwargs):
+        print("sale_init({}, {}, {})".format(amount, voucher, dummy))
+        obj = TransactionData()
+        cmd = STX + "0200|" + str(amount) + "|" + str(voucher[-6:]) + "|0|1" + ETX
+        cmd_hex = posutils.hex_string(cmd, crc=True)
+        flag = False
+        try:
+            result = obj.set_response(self.__execute(cmd_hex))
+            if result[0] == ACK:
+                result = obj.set_response(self.__wait_data()[2:-2])
+                flag = self.__get_flags(result, TX_RESPUESTA)
+            if flag:
+                obj.set_response_code(flag)
+                obj.set_text(self.__get_properties(flag))
+                if flag == "00":
+                    obj.result = True
+                    obj.add_content("num_voucher", voucher)  # VOUCHER INTERNO GENERADO POR MAPFRE
+                    obj.add_content("codigo_comercio", self.__get_flags(result, TX_CODIGO_COMERCIO))
+                    obj.add_content("terminal_id", self.__get_flags(result, TX_TERMINAL_ID))
+                    obj.add_content("num_voucher_mapfre", self.__get_flags(result, VENTA_TX_NUM_VOUCHER_MAPFRE))# VOUCHER MAPFRE RETORNADO POR TBK
+                    obj.add_content("codigo_autorizacion", self.__get_flags(result, VENTA_TX_CODIGO_AUTORIZACION))  # CODIGO AUTORIZACION TBK
+                    obj.add_content("num_cuotas", self.__get_flags(result, VENTA_TX_NUMERO_CUOTAS))
+                    obj.add_content("monto_cuota", self.__get_flags(result, VENTA_TX_MONTO_CUOTA))
+                    obj.add_content("ult_4_numeros", self.__get_flags(result, VENTA_TX_ULT_4_DIGITOS))
+                    obj.add_content("codigo_operacion", self.__get_flags(result, VENTA_TX_CODIGO_OPERACION))  # CODIGO OPERACION TBK
+                    obj.add_content("tipo_tarjeta", self.__get_flags(result, VENTA_TX_TIPO_TARJETA))
+                    obj.add_content("codigo_tarjeta", self.__get_flags(result, VENTA_TX_CODIGO_TARJETA))
+                    obj.add_content("fecha", self.__get_flags(result, VENTA_TX_FECHA))
+                    obj.add_content("hora", self.__get_flags(result, VENTA_TX_HORA))
+                else:
+                    obj.set_text(self.__get_properties(flag))
+            else:
+                obj.add_content("status", "FAIL")
             self.ack()
         except IOError as err:
             print("More errors: {}".format(err))
         return obj
-
+    
     def sale_detail(self):  # TODO check return data
         obj = TransactionData()
         cmd = STX + "0260|0" + ETX + "K"
@@ -111,7 +182,6 @@ class TbkPos(object):
                 obj.set_text("Detalle de ventas en Pos solicitado exitosamente.")
             else:
                 obj.set_text("Error al obtener detalle de ventas.")
-            self.ack()
         except IOError as err:
             print("More errors: {}".format(err))
         return obj
@@ -124,7 +194,6 @@ class TbkPos(object):
             result = obj.set_response(self.__execute(cmd_hex))
             obj.set_response_code(result[7:9])
             obj.set_text(self.__get_properties(result[7:9]))
-            self.ack()
         except IOError as err:
             print("More errors: {}".format(err))
         return obj
@@ -161,25 +230,6 @@ class TbkPos(object):
             print("More errors: {}".format(err))
         return obj
 
-    def load_keys(self):
-        obj = TransactionData()
-        cmd = STX + "0800" + ETX
-        cmd_hex = posutils.hex_string(cmd, crc=True)
-        try:
-            result = obj.set_response(self.__execute(cmd_hex))
-            while result == ACK:
-                result = obj.set_response(self.__execute(cmd_hex))
-            flag = self.__get_flags(result, TX_RESPUESTA)
-            obj.set_response_code(flag)
-            if flag == "00":
-                obj.result = True
-                obj.add_content("codigo_comercio", self.__get_flags(result, TX_CODIGO_COMERCIO))
-                obj.add_content("terminal_id", self.__get_flags(result, TX_TERMINAL_ID))
-            self.ack()
-        except IOError as err:
-            print("More errors: {}".format(err))
-        return obj
-
     def cancel_transaction(self):  # , **kwargs):
         obj = TransactionData()
         cmd = STX + "1200" + ETX
@@ -201,63 +251,4 @@ class TbkPos(object):
             print("More errors: {}".format(err))
         return obj
 
-    def sale_init(self, amount, voucher='0', dummy=False):  # , **kwargs):
-        print("sale_init({}, {}, {})".format(amount, voucher, dummy))
-        obj = TransactionData()
-        cmd = STX + "0200|" + str(amount) + "|" + str(voucher) + "|0|1" + ETX
-        cmd_hex = posutils.hex_string(cmd, crc=True)
-        if not dummy:
-            try:
-                result = obj.set_response(self.__execute(cmd_hex))
-                while result == ACK:
-                    result = obj.set_response(self.__execute(cmd_hex))
-                flag = self.__get_flags(result, TX_RESPUESTA)
-                if flag:
-                    obj.set_response_code(flag)
-                    obj.set_text(self.__get_properties(flag))
-                    if flag == "00":
-                        obj.result = True
-                        obj.add_content("num_voucher", voucher)  # VOUCHER INTERNO GENERADO POR MAPFRE
-                        obj.add_content("codigo_comercio", self.__get_flags(result, TX_CODIGO_COMERCIO))
-                        obj.add_content("terminal_id", self.__get_flags(result, TX_TERMINAL_ID))
-                        obj.add_content("num_voucher_mapfre", self.__get_flags(result, VENTA_TX_NUM_VOUCHER_MAPFRE))# VOUCHER MAPFRE RETORNADO POR TBK
-                        obj.add_content("codigo_autorizacion", self.__get_flags(result, VENTA_TX_CODIGO_AUTORIZACION))  # CODIGO AUTORIZACION TBK
-                        obj.add_content("num_cuotas", self.__get_flags(result, VENTA_TX_NUMERO_CUOTAS))
-                        obj.add_content("monto_cuota", self.__get_flags(result, VENTA_TX_MONTO_CUOTA))
-                        obj.add_content("ult_4_numeros", self.__get_flags(result, VENTA_TX_ULT_4_DIGITOS))
-                        obj.add_content("codigo_operacion", self.__get_flags(result, VENTA_TX_CODIGO_OPERACION))  # CODIGO OPERACION TBK
-                        obj.add_content("tipo_tarjeta", self.__get_flags(result, VENTA_TX_TIPO_TARJETA))
-                        obj.add_content("codigo_tarjeta", self.__get_flags(result, VENTA_TX_CODIGO_TARJETA))
-                        obj.add_content("fecha", self.__get_flags(result, VENTA_TX_FECHA))
-                        obj.add_content("hora", self.__get_flags(result, VENTA_TX_HORA))
-                else:
-                    obj.set_text(self.__get_properties(flag))
-                    obj.add_content("status", "FAIL")
-                self.ack()
-            except IOError as err:
-                print("More errors: {}".format(err))
-        else:  # FIXME check for dummy values.
-            from random import randint
-            from datetime import datetime
-            obj.result = True
-            quote = randint(1, 4)
-            now = datetime.now()
-            fecha = now.strftime("%Y-%m-%d")
-            hora = now.strftime("%H:%M")
-            last_dig = '{:0>4}'.format(str(randint(0, 9999)))
-            obj.add_content("num_voucher", randint(1,voucher))  # VOUCHER INTERNO GENERADO POR MAPFRE
-            obj.add_content("codigo_comercio", "655550")
-            obj.add_content("terminal_id", "223")
-            obj.add_content("num_voucher_mapfre", voucher)  # VOUCHER MAPFRE RETORNADO POR TBK
-            obj.add_content("codigo_autorizacion", "6556")  # CODIGO AUTORIZACION TBK
-            obj.add_content("num_cuotas", quote)
-            obj.add_content("monto_cuota", amount//quote)
-            obj.add_content("ult_4_numeros", last_dig)
-            obj.add_content("codigo_operacion","555664")
-            obj.add_content("tipo_tarjeta", "D")
-            obj.add_content("codigo_tarjeta", "D")
-            obj.add_content("fecha", fecha)
-            obj.add_content("hora", hora)
-            obj.add_content("status", "OK")
-            sleep(30)
-        return obj
+
