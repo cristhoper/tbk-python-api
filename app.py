3ffb906b21afacfbef4a24edf41d509106f65e84
Cristhoper <cristhoper@gmail.com>
2019-07-09 16:25:31 -0400

    Check...

--- app.py
+++ app.py
@@ -96,8 +96,8 @@ def worker_sale(amount, transaction_id, dummy=False):
     transactions_in_progress[transaction_id] = None
     pos_status = pos.sale_init(amount, transaction_id, dummy)
     transactions_in_progress[transaction_id] = pos_status
-    print("Worker ended")
     print_html_data(pos_status)
+    print("Worker ended")
     safe_pos.release()
 
 
